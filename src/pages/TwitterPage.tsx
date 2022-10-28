e12e97cd54ebd2b39372e6668c25899b87e611e9
Oliver Klingefjord <oliver@kry.se>
2022-10-28 14:32:27 +0200

    deafult topic matching list

--- src/pages/TwitterPage.tsx
+++ src/pages/TwitterPage.tsx
@@ -14,12 +14,45 @@ export type TwitterHyperParams = {
   similarity_threshold: number;
 };
 
+function defaultTopic(listId: string): string {
+  switch (listId) {
+    case "1174203958044348418":
+    case "1483456727219683332":
+      return "Geopolitics";
+    case "1229800319527223296":
+      return "Meditation";
+    case "1280729707994988545":
+      return "NLP";
+    case "1285998958012178434":
+      return "Computer Graphics";
+    case "1413729015454568454":
+    case "1518268962953482241":
+      return "Psychedelics";
+    case "1414394668276600832":
+      return "Effective Altruism";
+    case "1471523818506276866":
+      return "Aave";
+    case "1494852872881811461":
+    case "1512385400190480396":
+    case "1550512678980014081":
+      return "Crypto";
+    case "1511977355505512450":
+    case "1558460052645617664":
+    case "1570063337723166720":
+      return "Web 3";
+    case "1548015533710123010":
+      return "Permaculture";
+    default:
+      return "Technology";
+  }
+}
+
 function TwitterPage() {
   const { list_id } = useParams();
 
   const [hyperParams, setHyperParams] = useState<TwitterHyperParams>({
     list_id: list_id!!,
-    topic: "Permaculture",
+    topic: defaultTopic(list_id!!),
     alpha: 0.85,
     sentiment_weight: 0.2,
     similarity_threshold: 0.5,
